aa6dbca2d532655817bece36079862b216fedb88
McPiotro <mcpiotro@o2.pl>
2023-02-21 09:56:01 +0100

    Dzień 5

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,39 +1,71 @@
-﻿var name = "Malia";
-string gender = "Kobieta";
-string gender2 = "Mężczyzna";
-int age = 35;
-int age2 = 17;
+﻿int number = 928911;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
+
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
 
-
-if (age == 35 && gender == "Kobieta" && name == "Lola")
+foreach (char letter in letters)
 {
-    Console.WriteLine("-Kobieta o imieniu Lola, lat 35");
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
+Console.WriteLine("Liczba " + (number) + " zawiera:");
 
-else if (age == 35 && gender == "Kobieta" && name == "Malia")
-{
-    Console.WriteLine("-Kobieta o imieniu Malia, lat 35");
-}
-
-
-if (gender == "Kobieta" && age < 30)
-{
-    Console.WriteLine("-Kobieta przed 30 lat");
-}
-
-else if (gender == "Kobieta" && age > 30)
-{
-    Console.WriteLine("-Kobieta powyżej 30 lat");
-}
-
-
-if (gender2 == "Mężczyzna" && age2 >= 18)
-{
-    Console.WriteLine("-Pełnoletni Mężczyzna");
-}
-
-else if (gender2 == "Mężczyzna" && age2 < 18)
-{
-    Console.WriteLine("-Niepełnoletni Mężczyzna");
-}
+Console.WriteLine("Cyfrę '0' -> " + counter0 + " razy ");
+Console.WriteLine("Cyfrę '1' -> " + counter1 + " razy ");
+Console.WriteLine("Cyfrę '2' -> " + counter2 + " razy ");
+Console.WriteLine("Cyfrę '3' -> " + counter3 + " razy ");
+Console.WriteLine("Cyfrę '4' -> " + counter4 + " razy ");
+Console.WriteLine("Cyfrę '5' -> " + counter5 + " razy ");
+Console.WriteLine("Cyfrę '6' -> " + counter6 + " razy ");
+Console.WriteLine("Cyfrę '7' -> " + counter7 + " razy ");
+Console.WriteLine("Cyfrę '8' -> " + counter8 + " razy ");
+Console.WriteLine("Cyfrę '9' -> " + counter9 + " razy ");
